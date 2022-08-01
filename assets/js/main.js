9d1c4444fdb4a8d49303dd60b0f31b5778429e0f
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-08-01 22:25:12 +0500

    Header and search completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,9 @@ window.addEventListener("scroll", scrollHeader);
 // Open menu & search pop-up
 
 const menuToggleIcon = selectElement("#menu-toggle-icon");
+const formOpenBtn = selectElement("#search-icon");
+const formCloseBtn = selectElement("#form-close-btn");
+const searchContainer = selectElement("#search-form-container");
 
 const toggleMenu = () => {
   const mobileMenu = selectElement("#menu");
@@ -29,13 +32,30 @@ const toggleMenu = () => {
 };
 
 menuToggleIcon.addEventListener("click", toggleMenu);
+
 // Open/Close search form popup
 
+formOpenBtn.addEventListener("click", () =>
+  searchContainer.classList.add("activated")
+);
+formCloseBtn.addEventListener("click", () =>
+  searchContainer.classList.remove("activated")
+);
+
 // -- Close the search form popup on ESC keypress
+
+window.addEventListener("keyup", (event) => {
+  if (event.key === "Escape") searchContainer.classList.remove("activated");
+});
 
 // Switch theme/add to local storage
 const bodyElement = document.body;
 const themeToggleBtn = selectElement("#theme-toggle-btn");
+const currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+  bodyElement.classList.add("light-theme");
+}
 
 themeToggleBtn.addEventListener("click", () => {
   bodyElement.classList.toggle("light-theme");
@@ -46,4 +66,31 @@ themeToggleBtn.addEventListener("click", () => {
     localStorage.removeItem("currentTheme");
   }
 });
-// Swiper
+// Swiper.js
+
+const swiper = new Swiper(".swiper", {
+  // How many slides to show
+  slidesPerView: 1,
+  // How much space between slides
+  spaceBetween: 20,
+  // Make the next and previous buttons work
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Make the pagination indicators work
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  //Responsive breakpoints for how many slides to show at that view
+  breakpoints: {
+    // 700px and up shoes 2 slides
+    700: {
+      slidesPerView: 2,
+    },
+    // 1200px and up shoes 3 slides
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
